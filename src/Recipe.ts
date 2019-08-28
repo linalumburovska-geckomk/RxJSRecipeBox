bcf8ef64417672cf93b92baaeebaf9643d600788
Lina Lumburovska <lina.lumburovska@gecko.mk>
2019-08-28 16:57:53 +0200

    Delete recipes added

--- src/Recipe.ts
+++ src/Recipe.ts
@@ -17,16 +17,21 @@ class Recipe {
 
         // Set an empty array for all recipes
         let array: any = JSON.parse(sessionStorage.getItem('allRecipes') || "[]");
-        if(array!="[]") {
-            for(let i =0; i < array.length;i++) {
-                let row = this.table.insertRow(i+1);
-                row.insertCell(0).innerText = array[i].id;
-                row.insertCell(1).innerText = array[i].name;
-                row.insertCell(2).innerText = array[i].ingredients;
-            }
-        } else {
-            sessionStorage.setItem('allRecipes',JSON.stringify(array));
+        for(let i =0; i < array.length;i++) {
+            let row = this.table.insertRow(i);
+            row.insertCell(0).innerText = array[i].name;
+            row.insertCell(1).innerText = array[i].ingredients;
+
+            let deleteButton = document.createElement('button');
+            deleteButton.setAttribute('class', 'btn btn-info');
+            deleteButton.innerText='Delete';
+
+            row.insertCell(2).append(deleteButton);
+
+            fromEvent(deleteButton, 'click').subscribe(() => this.deleteRecipe(i));
+
         }
+
     }
 
     private selectElements = () : void => {
@@ -34,11 +39,13 @@ class Recipe {
       this.nameNewRecipe = this.formNewRecipe.querySelector('.name');
       this.ingredientsNewRecipe = this.formNewRecipe.querySelector('.ingredients');
       this.submitNewRecipe = document.querySelector('.modal-footer button');
-      this.table = document.querySelector('table');
+      this.table = document.querySelector('table tbody');
+
     };
 
     private addEventListeners = () : void => {
         fromEvent(this.submitNewRecipe, 'click').subscribe(() => this.addNewRecipe());
+
     };
 
     private addNewRecipe = () : void => {
@@ -50,15 +57,31 @@ class Recipe {
             ingredients: this.ingredientsNewRecipe.value
         };
 
-        let row = this.table.insertRow(array.length+1);
-        row.insertCell(0).innerText = newItem.id;
-        row.insertCell(1).innerText = newItem.name;
-        row.insertCell(2).innerText = newItem.ingredients;
+        let row = this.table.insertRow(array.length);
+        row.insertCell(0).innerText = newItem.name;
+        row.insertCell(1).innerText = newItem.ingredients;
+
+        let deleteButton = document.createElement('button');
+        deleteButton.setAttribute('class', 'btn btn-info');
+        deleteButton.innerText='Delete';
+
+        row.insertCell(2).append(deleteButton);
+        fromEvent(deleteButton, 'click').subscribe(() => this.deleteRecipe(newItem.id));
 
         array.push(newItem);
         sessionStorage.setItem('allRecipes', JSON.stringify(array));
     };
 
+    private deleteRecipe = ( idx : number) : void => {
+        let array = JSON.parse(sessionStorage.getItem('allRecipes'));
+        for( let i = 0; i < array.length; i++){
+            if (i == idx) {
+                array.splice(i, 1);
+                this.table.deleteRow(idx);
+            }
+        }
+        sessionStorage.setItem('allRecipes', JSON.stringify(array));
+    };
 }
 
 export default Recipe;
