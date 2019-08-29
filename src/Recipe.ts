2a25c30f75c11b9b2768e1e0de23c65599b44650
LinaLumburovska <lina.lumburovska@gecko.mk>
2019-08-29 16:49:33 +0200

    Edit implemented

--- src/Recipe.ts
+++ src/Recipe.ts
@@ -1,4 +1,4 @@
-import { fromEvent } from 'rxjs';
+import {from, fromEvent} from 'rxjs';
 import 'bootstrap';
 
 class Recipe {
@@ -10,9 +10,11 @@ class Recipe {
     ingredientsNewRecipe: HTMLInputElement;
     table: HTMLTableElement;
     viewRecipeModal: HTMLDivElement;
-    viewRecipeModalTitle: HTMLDivElement;
     viewRecipeModalName: HTMLDivElement;
     viewRecipeModalIngredients: HTMLDivElement;
+    editButton: HTMLButtonElement;
+    saveButton: HTMLButtonElement;
+    closeButton: HTMLButtonElement;
 
     constructor() {
 
@@ -26,7 +28,7 @@ class Recipe {
             row.insertCell(0).innerText = array[i].name;
             row.insertCell(1).innerText = array[i].ingredients;
 
-            fromEvent(row.firstChild, 'click').subscribe(()=> this.viewRecipe(row));
+            fromEvent(row.firstChild, 'click').subscribe(()=> this.viewRecipe(row,i));
 
             let deleteButton = document.createElement('button');
             deleteButton.setAttribute('class', 'btn btn-info');
@@ -47,14 +49,15 @@ class Recipe {
       this.submitNewRecipe = document.querySelector('.modal-footer button');
       this.table = document.querySelector('table tbody');
       this.viewRecipeModal = document.querySelector('#viewRecipe');
-      this.viewRecipeModalTitle = this.viewRecipeModal.querySelector('.modal-title');
       this.viewRecipeModalName = this.viewRecipeModal.querySelector('.modal-body label.name');
       this.viewRecipeModalIngredients = this.viewRecipeModal.querySelector('.modal-body label.ingredients');
+      this.editButton = this.viewRecipeModal.querySelector('.modal-footer button.edit');
+      this.saveButton = this.viewRecipeModal.querySelector('.modal-footer button.save');
+      this.closeButton = this.viewRecipeModal.querySelector('.close');
     };
 
     private addEventListeners = () : void => {
         fromEvent(this.submitNewRecipe, 'click').subscribe(() => this.addNewRecipe());
-
     };
 
     private addNewRecipe = () : void => {
@@ -70,7 +73,7 @@ class Recipe {
         row.insertCell(0).innerText = newItem.name;
         row.insertCell(1).innerText = newItem.ingredients;
 
-        fromEvent(row.firstChild, 'click').subscribe((e)=> this.viewRecipe(row));
+        fromEvent(row.firstChild, 'click').subscribe((e)=> this.viewRecipe(row,newItem.id));
 
         let deleteButton = document.createElement('button');
         deleteButton.setAttribute('class', 'btn btn-info');
@@ -94,17 +97,13 @@ class Recipe {
         sessionStorage.setItem('allRecipes', JSON.stringify(array));
     };
 
-    private viewRecipe = (row: ChildNode) : void => {
+    private viewRecipe = (row: ChildNode, idx: number) : void => {
         this.viewRecipeModal.setAttribute('class', 'modal fade show');
         this.viewRecipeModal.style.display="block";
         this.body.setAttribute('class', 'modal-open');
         let div = document.createElement('div');
         div.setAttribute('class','modal-backdrop fade show');
         this.body.append(div);
-
-        let spanTitle = document.createElement('span');
-        spanTitle.innerText = row.childNodes[0].textContent;
-        this.viewRecipeModalTitle.append(spanTitle);
 
         let spanName = document.createElement('span');
         spanName.innerText = row.childNodes[0].textContent;
@@ -114,7 +113,57 @@ class Recipe {
         spanIngredients.innerText = row.childNodes[1].textContent;
         this.viewRecipeModalIngredients.append(spanIngredients);
 
-    }
+        fromEvent(this.editButton, 'click').subscribe(()=> this.editRecipe(row,idx));
+        fromEvent(this.closeButton, 'click').subscribe(() => this.closeModal());
+    };
+
+    private editRecipe = (row: ChildNode, idx: number) : void => {
+        this.editButton.classList.add('d-none');
+        this.saveButton.classList.remove('d-none');
+
+        this.viewRecipeModalName.childNodes[1].remove();
+        let inputName = document.createElement('input');
+        this.viewRecipeModalName.append(inputName);
+        inputName.value = row.childNodes[0].textContent;
+
+        this.viewRecipeModalIngredients.childNodes[1].remove();
+        let inputIngredients = document.createElement('input');
+        this.viewRecipeModalIngredients.append(inputIngredients);
+        inputIngredients.value = row.childNodes[1].textContent;
+
+        fromEvent(this.saveButton, 'click').subscribe(() => this.saveRecipe(row,idx,inputName.value,inputIngredients.value));
+        fromEvent(this.closeButton, 'click').subscribe(() => this.closeModal());
+    };
+
+    private saveRecipe = (row: ChildNode, idx: number, newName: string, newIngredients: string) : void => {
+        let array = JSON.parse(sessionStorage.getItem('allRecipes'));
+        for(let i=0; i<array.length; i++) {
+            if(idx == i){
+                array[idx].name = newName;
+                array[idx].ingredients = newIngredients;
+            }
+        }
+        sessionStorage.setItem('allRecipes', JSON.stringify(array));
+        row.childNodes[0].textContent = newName;
+        row.childNodes[1].textContent = newIngredients;
+
+        this.closeModal();
+    };
+
+    private closeModal = () : void => {
+        this.viewRecipeModal.setAttribute('class', 'modal fade');
+        this.viewRecipeModal.style.display="none";
+        this.body.classList.remove('modal-open');
+        this.body.querySelector('.modal-backdrop').remove();
+
+        this.viewRecipeModal.querySelectorAll('input').forEach((input: HTMLInputElement): void => {
+            input.remove();
+        });
+        this.saveButton.classList.add('d-none');
+        this.editButton.classList.remove('d-none');
+
+    };
+
 
 }
 
